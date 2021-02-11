ad155ddfd04cd8e2735fcfc974190dc7a0e52c59
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-11 20:13:41 +0530

    wrote basic function to App.ts

---
+++ src/main/kotlin/spectrum/App.kts
@@ -0,0 +1,12 @@
+public class Server {
+    fun hello(args: Array<String>) {
+        print("Hello what's your good name? ")
+
+        val enteredString = readLine()
+        printLn("What would you like to do today $enteredString:" +
+                "1) Create new Chat Session (1)" +
+                "2) Join existing Chat Session (2)")
+    }
+
+    func
+}
