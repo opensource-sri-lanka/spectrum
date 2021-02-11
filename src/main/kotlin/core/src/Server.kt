f0c597aa148e77ae7db78907d69de4012066853f
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-11 20:53:16 +0530

    server socket.io config

--- src/main/kotlin/core/src/Server.kt
+++ src/main/kotlin/core/src/Server.kt
@@ -4,3 +4,24 @@ import com.github.nkzawa.socketio.client.IO
 import com.github.nkzawa.socketio.client.Socket
 import java.net.URISyntaxException
 
+// get the socket URL
+
+class SocketInstance : Application() {
+    // socket.io connection url
+    private var mSocket: Socket? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            // creating socket instance
+            mSocket = IO.socket(URL)
+        } catch (e: java.net.URISyntaxException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    // return socket instance
+    fun getMSocket(): Socket? {
+        return mSocket
+    }
+}
