f26b9831596c79fbe40c2b02b183db5f057d22bb
Tarith <tarithj@gmail.com>
2021-02-11 14:13:23 +0530

    moved files

--- Account/src/main.kt
+++
@@ -1,9 +0,0 @@
-import com.github.nkzawa.emitter.Emitter
-import com.github.nkzawa.socketio.client.IO
-import com.github.nkzawa.socketio.client.Socket
-
-val socket = IO.socket("http://localhost:/8080")
-
-socket.on(Socket.EVENT_CONNECT, Emitter.Listener {
-    socket.emit("messages", "hi")
-});
