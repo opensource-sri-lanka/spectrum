a1c6e478becc21e5a7a08c7a4217fd1770fd2a50
Tarith <tarithj@gmail.com>
2021-02-11 13:52:25 +0530

    created dummy hash and verify methods for Session

--- Account/src/models/Session.kt
+++ Account/src/models/Session.kt
@@ -1,4 +1,14 @@
 class Session {
-    name: String,
-    password: String
+    var name: String,
+    var password: String
+
+    // hashes the password and saves it in session
+    fun StoreHashedPassowrd(password:String): Boolean {
+
+    }
+
+    // verifies if the password is correct
+    fun VerifyPassword(password: String): Boolean{
+
+    }
 }
